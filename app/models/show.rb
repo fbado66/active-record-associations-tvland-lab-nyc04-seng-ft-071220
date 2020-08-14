9cc43cdc126ba006287e0045a23786c796c73212
fbado66 <fabdo66@gmail.com>
2020-08-13 22:43:39 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
 class Show < ActiveRecord::Base
+
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        # binding.pry
+        # Actor.all.map {|actor| "#{actor.first_name} #{actor.last_name}" }
+
+        # alternative 
+        Actor.all.map(&:full_name)
+    end 
+
+
+
   
 end
